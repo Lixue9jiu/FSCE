310b232ed89ff428650e1d61b4f8237c827803f3
Lixue_jiu <lixue9jiu@users.noreply.github.com>
2018-05-13 13:21:18 -0400

    修复手机版里操作板遮住暂停界面的问题

---
+++ Assets/_Scripts/Utils/ButtonClickHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Button))]
+public class ButtonClickHandler : MonoBehaviour, IPointerUpHandler
+{
+	public UnityEngine.Events.UnityEvent PointerUpHandler;
+
+	public void OnPointerUp(PointerEventData eventData)
+	{
+		PointerUpHandler.Invoke();
+	}
+}
