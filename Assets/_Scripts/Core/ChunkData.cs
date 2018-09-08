bad26496100dffa1866b859811a83123c11b0edd
Lixue_jiu <lixue9jiu@gmail.com>
2018-09-08 22:04:21 +0800

    升级前备份

---
+++ Assets/_Scripts/Core/ChunkData.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkData
+{
+    const int terrainSize = 8;
+    const int chunkSize = 32768;
+
+    int[][] chunks;
+
+    ChunkIndexer chunkIndexer;
+
+    public ChunkData()
+    {
+        chunks = new int[terrainSize * terrainSize][];
+        chunkIndexer = new ChunkIndexer(terrainSize);
+    }
+
+    public int[] GetChunk(int index)
+    {
+        return chunks[index];
+    }
+
+    public int GetBlock(int x, int y, int z)
+    {
+        return chunks[chunkIndexer.GetChunkIndex(x >> 4, z >> 4)][GetBlockIndex(x & 15, y & 127, z & 15)];
+    }
+
+    public int GetBlockIndex(int x, int y, int z)
+    {
+        return y + x << 4 + z << 11;
+    }
+
+    public int[] CreateChunk(int index)
+    {
+        if (chunks[index] == null)
+            chunks[index] = new int[chunkSize];
+        return chunks[index];
+    }
+
+    public void DestoryChunk(int index)
+    {
+        chunks[index] = null;
+    }
+
+    public static bool GetBlockNeighbors(int x, int y, int z, int blockIndex, int[] currentChunk, int[][] chunks, ref int[] neighbors)
+    {
+        if (y == 127 || y == 0)
+            return true;
+
+        neighbors[CellFace.BOTTOM] = currentChunk[blockIndex - 1];
+        neighbors[CellFace.TOP] = currentChunk[blockIndex + 1];
+
+        neighbors[CellFace.FRONT] = currentChunk[blockIndex + 16];
+        neighbors[CellFace.BACK] = currentChunk[blockIndex - 16];
+
+        neighbors[CellFace.LEFT] = currentChunk[blockIndex - 16];
+        neighbors[CellFace.RIGHT] = currentChunk[blockIndex - 16];
+        return false;
+    }
+}
---
+++ Assets/_Scripts/Core/ChunkIndexer.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChunkIndexer
+{
+    public readonly int size;
+    public readonly int halfSize;
+    int mask;
+    int shift;
+
+    public int offsetX;
+    public int offsetY;
+
+    public ChunkIndexer(int size)
+    {
+        this.size = size;
+        halfSize = size / 2;
+        mask = size - 1;
+        shift = Log2(size);
+    }
+
+    int Log2(int x)
+    {
+        int i = -1;
+        while (x != 0)
+        {
+            i++;
+            x = x >> 1;
+        }
+        return i;
+    }
+
+    public void SetOffset(int x, int y)
+    {
+        //Debug.Log(string.Format("set offset to: {0}, {1}", x, y));
+        offsetX = x;
+        offsetY = y;
+    }
+
+    public void Offset(int x, int y)
+    {
+        offsetX += x;
+        offsetY += y;
+    }
+
+    public int GetChunkIndex(int x, int y)
+    {
+        return (x & mask) + ((y & mask) << shift);
+    }
+
+    public bool IsValid(int x, int y)
+    {
+        x -= offsetX;
+        y -= offsetY;
+        return x > -1 && x < size && y > -1 && y < size;
+    }
+
+    public void GetOriginal(ref int x, ref int y)
+    {
+        x = ((x - offsetX) & mask) + offsetX;
+        y = ((y - offsetY) & mask) + offsetY;
+    }
+}
+
+public struct ChunkStat
+{
+    
+}
